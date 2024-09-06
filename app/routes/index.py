7e46398f3e4a1964d4e2315090f3185ea8760c9c
dadadaguai <1183984923@qq.com>
2024-09-06 15:50:03 +0800

    完成了关键字提取API的调用与实现。

--- app/routes/index.py
+++ app/routes/index.py
@@ -70,9 +70,9 @@ def get_users():
 
 # 在这里可以添加更多的 API 路由...
 
-
-@bp.route('/add_pomodoro', methods=['POST'])
-def add_pomodoro():
+# 正常结束的番茄钟请求
+@bp.route('/add_normal_pomodoro', methods=['POST'])
+def add_normal_pomodoro():
     data = request.get_json()
     user_id = data.get('UserID')
     start_time_str = data.get('StartTime')
@@ -92,13 +92,18 @@ def add_pomodoro():
         end_time = datetime.fromisoformat(end_time_str).replace(tzinfo=pytz.utc)
     except ValueError:
         return jsonify({'message': '无效的时间格式'}), 400
-
+    break_duration = 0
+    if completed:
+        is_break_duration = (end_time - start_time) / 1000 - duration
+        if is_break_duration > 0:
+            break_duration = is_break_duration  # 记录番茄的预定时间和实际时间的差值。
     # 创建番茄钟会话
     pomodoro_session = PomodoroSession(
         user_id=user_id,
         start_time=start_time,
         end_time=end_time,
         duration=duration,
+        break_duration=break_duration,
         completed=completed
     )
     db.session.add(pomodoro_session)
@@ -120,4 +125,51 @@ def add_pomodoro():
         'message': '番茄钟添加成功',
         'pomodoro_session_id': pomodoro_session.id,
         'pomodoro_summary_id': pomodoro_summary.id
+    }), 201
+
+# 没有笔记的请求
+@bp.route('/add_pomodoro_without_summary', methods=['POST'])
+def add_pomodoro_without_summary():
+    data = request.get_json()
+    user_id = data.get('UserID')
+    start_time_str = data.get('StartTime')
+    end_time_str = data.get('EndTime')
+    duration = data.get('Duration')
+    completed = data.get('Completed', False)  # 默认为False
+
+    # 验证用户是否存在
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return jsonify({'message': '用户不存在'}), 404
+
+    # 将时间字符串转换为 datetime 对象
+    try:
+        start_time = datetime.fromisoformat(start_time_str).replace(tzinfo=pytz.utc)
+        end_time = datetime.fromisoformat(end_time_str).replace(tzinfo=pytz.utc)
+    except ValueError:
+        return jsonify({'message': '无效的时间格式'}), 400
+
+    break_duration = 0
+    if completed:
+        is_break_duration = (end_time - start_time) / 1000 - duration
+        if is_break_duration > 0:
+            break_duration = is_break_duration  # 记录番茄的预定时间和实际时间的差值。
+
+    # 创建番茄钟会话
+    pomodoro_session = PomodoroSession(
+        user_id=user_id,
+        start_time=start_time,
+        end_time=end_time,
+        duration=duration,
+        break_duration=break_duration,
+        completed=completed
+    )
+    db.session.add(pomodoro_session)
+
+    # 提交数据库会话以生成PomodoroSession的id
+    db.session.commit()
+
+    return jsonify({
+        'message': '番茄钟添加成功',
+        'pomodoro_session_id': pomodoro_session.id
     }), 201
